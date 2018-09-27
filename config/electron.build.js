d6f8f1142df4db9a8991998174e48b773f64ecde
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2018-09-27 15:33:32 +0530

    [added] generating app icon from png

--- config/electron.build.js
+++ config/electron.build.js
@@ -1,40 +1,6 @@
 const path = require("path")
 const _package = require("../package.json")
 
-const build = {
-	productName: _package.appName,
-	appId: _package.appId,
-	copyright: "© " + (new Date()).getUTCFullYear + " " + _package.author,
-	directories: {
-		output: "dist"
-	},
-	files: [ "dist/electron/**/*",  ],
-	dmg: {
-		contents: [
-			{
-				x: 410,
-				y: 150,
-				type: "link",
-				path: "/Applications"
-			},
-			{
-				x: 130,
-				y: 150,
-				type: "file"
-			},
-		]
-	},
-	mac: {
-		icon: "build/icons/icon.icns"
-	},
-	win: {
-		icon: "build/icons/icon.ico"
-	},
-	linux: {
-		icon: "build/icons"
-	}
-}
-
 /**
  * `electron-packager` options
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-packager.html
