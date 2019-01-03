dc8926e63aaae9629120ae77c9996f8f78361394
Kundan Singh <kundan785@gmail.com>
2019-01-03 19:33:15 +0530

    ui change

--- src/app/app.vue
+++ src/app/app.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<v-app dark>
 			<dev-resizer :show="showDevResizer" />
-			<v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="true" v-model="drawer" app >
+			<!--v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="true" v-model="drawer" app >
 				<v-list>
 					<v-list-tile router :to="item.to" :key="i" v-for="(item, i) in items" exact>
 						<v-list-tile-action>
@@ -19,7 +19,73 @@
 				<v-toolbar-title v-text="title"></v-toolbar-title>
 				<v-spacer />
 				<app-exit-button />
-			</v-toolbar>
+			</v-toolbar-->
+			<v-navigation-drawer fixed :clipped="$vuetify.breakpoint.mdAndUp" app v-model="drawer">
+      			<v-list dense>
+        			<template v-for="item in items">
+          				<v-layout row v-if="item.heading" align-center :key="item.heading">
+            				<v-flex xs6>
+              					<v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            				</v-flex>
+            				<v-flex xs6 class="text-xs-center">
+              					<a href="#!" class="body-2 black--text">EDIT</a>
+            				</v-flex>
+          				</v-layout>
+          			<v-list-group v-else-if="item.children" v-model="item.model" :key="item.text" :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
+            			<v-list-tile slot="activator">
+              				<v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else :key="item.text">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="orange darken-4" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
+			<span class="hidden-sm-and-down" style="width: 65px" >GymKonnect</span>
+      <v-toolbar-title style="width: 100px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      </v-toolbar-title>
+			<v-spacer></v-spacer>
+      <v-text-field flat solo-inverted prepend-icon="search" label="Search" class="hidden-sm-and-down pt-2" />
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+       <v-avatar color="orange darken-4" size="40">
+      <v-icon dark>account_circle</v-icon>
+    </v-avatar>
+      </v-btn>
+    </v-toolbar>
 			<v-content>
 				<v-container fluid fill-height>
 					<v-slide-y-transition mode="out-in">
@@ -37,8 +103,11 @@
 					</v-list-tile>
 				</v-list>
 			</v-navigation-drawer>
-			<v-footer :fixed="fixed" app>
-				<v-spacer />
+			<v-footer dark :fixed="fixed" app>
+				<v-btn round>
+					<span>BackUp</span>
+				</v-btn>
+				<v-spacer></v-spacer>
 				<span> 
 					<v-chip color="grey darken-4" :label="true" :disabled="true" text-color="white">Messages Left :
       					
@@ -80,7 +149,7 @@ import devResizer from "@/components/dev-resizer.vue"
 export default class Vuetify extends Vue{
 	clipped: boolean = false
 	showDevResizer: boolean = false
-	drawer: boolean = true
+	drawer: boolean = false
 	fixed: boolean = false
 	miniVariant: boolean = false
 	right: boolean = true
