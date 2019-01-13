1466a07a2ab6a9f1a3cc10ce2c5e2557115644ef
Kundan Singh <kundan785@gmail.com>
2019-01-13 23:07:45 +0530

    Addons[start]

--- src/app/pages/registrations.vue
+++ src/app/pages/registrations.vue
@@ -1,6 +1,34 @@
 <template>
 	<Layout>
 		<h1 class="text-md-center">Registrations</h1>
+		<div>
+    <v-tabs v-model="active" color="cyan  darken-4" dark slider-color="yellow">
+      <v-tab v-for="(name, key) in tabsList" :key="key" ripple>
+        {{ name }}
+      </v-tab>
+      <v-tab-item key="a">
+        <v-card flat>
+          <span>yeah</span>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item key="b">
+        <v-card flat>
+          <span>yeah b</span>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item key="c">
+        <v-card flat>
+          <span>yeah c</span>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+
+    <div class="text-xs-center mt-3">
+      <v-btn @click="next">
+        next tab
+      </v-btn>
+    </div>
+  </div>
 			
 	</Layout>
 </template>
@@ -20,6 +48,16 @@ import { Component } from "vue-property-decorator"
 	},
 })
 export default class Home extends Vue{
+	active: number = 0
+	tabsList = {
+		a: "a",
+		b: "b",
+		c: "c",
+	}
+	 next() {
+        const active = parseInt(this.active.toString())
+        this.active = (active < 2 ? active + 1 : 0)
+      }
 	
 }
 </script>
