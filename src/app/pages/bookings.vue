e7808060c21f8f65f238661717996734138a0b19
K Karthikeyan <kkarthik24.2012@gmail.com>
2019-01-24 12:29:26 +0530

    ground booking

--- src/app/pages/bookings.vue
+++ src/app/pages/bookings.vue
@@ -10,7 +10,7 @@
         			<v-card color="#303030">
 						<v-layout row wrap>
 							<v-flex class="pl-2 ml-2" xs12>
-								<v-radio-group row label="SPA Booking Type: " v-model="radioTop" :mandatory="false">
+								<v-radio-group row label="SPA Booking Type: " v-model="radioTop">
 									<v-layout row align-start>
 										<v-radio class="" label="Solo" value="radio-1"></v-radio>
 										<v-radio class="" label="Group" value="radio-2"></v-radio>
@@ -97,8 +97,124 @@
       			</v-tab-item>
 
 				<v-tab-item key="b">
-        			<v-card flat color="#303030">
-						<span>ground booking coming soon</span>
+        			<v-card color="#303030">
+						<v-layout class="pt-4" row wrap>
+							<v-flex xs2>
+								<v-subheader class="title">Mobile No</v-subheader>
+							</v-flex>
+							<v-flex xs4>
+								<v-text-field label="Enter Mobile No" single-line solo></v-text-field>
+							</v-flex>	
+						</v-layout>
+						<v-divider></v-divider>
+
+						<v-layout class="pl-4 pt-2" row wrap>
+							<v-flex lg3 class="mr-3">
+								<v-text-field prepend-icon="fas fa-user" counter maxlength="15" label="First Name" required></v-text-field>
+							</v-flex>
+							<v-flex lg3 class="mr-3">
+								<v-text-field counter maxlength="15" label="Middle Name"></v-text-field>
+							</v-flex>
+							<v-flex lg3 class="mr-3">
+								<v-text-field counter maxlength="15" label="Last Name" required></v-text-field>
+							</v-flex>
+						</v-layout>
+
+						<v-layout class="pl-4 pt-2" row wrap>
+							<v-flex xs6 lg6>
+								<v-text-field prepend-icon="fas fa-building" label="Organization Name" required></v-text-field>
+							</v-flex>
+
+							<v-flex xs6>
+								<v-select prepend-icon="list" class="pr-4 pl-4" :items="OrgType" label="Organization Type"></v-select>
+							</v-flex>	
+						</v-layout>
+
+						<v-card class="pa-2" color="#303030">
+							<v-subheader class="title">Slot Booking</v-subheader>
+							<v-layout class="pl-4" row wrap>
+								<v-flex xs12>
+									<v-radio-group prepend-icon="fas fa-cookie" label="Ground Booking (2 Hrs Slot)" v-model="radioGB1" row>
+										<v-radio label="Without Floodlight" value="radio-wf"></v-radio>
+										<v-radio label="With Floodlight" value="radio-f"></v-radio>
+										<v-dialog v-model="dialogSlot" persistent max-width="600px">
+											<span slot="activator" class="subheading orange--text">Select Slot</span>
+											<v-card>
+												<v-toolbar card dark color="orange darken-4">
+													<v-toolbar-title>Select the Time Slot</v-toolbar-title>
+												</v-toolbar>
+												<v-card-actions>
+													<v-layout row wrap>
+														<v-flex xs12 class="py-2">
+															<p>
+																<span class="green--text pl-4">Available</span>
+																<span class="red--text pl-4">Booked</span>
+																<span class="yellow--text pl-4">Reserved</span>
+															</p>
+															<v-btn-toggle v-model="toggle_none">
+																<v-btn color="green">6am-8am</v-btn>
+																<v-btn color="red">8am-10am</v-btn>
+																<v-btn color="green">10am-12pm</v-btn>
+																<v-btn color="green">12pm-2pm</v-btn>
+																<v-btn color="green">2pm-4pm</v-btn>
+																<v-btn color="yellow">4pm-6pm</v-btn>
+															</v-btn-toggle>
+														</v-flex>
+
+														<v-flex xs12>
+															<v-spacer></v-spacer>
+															<v-btn color="orange darken-4" @click="dialogSlot = false">Confirm</v-btn>
+															<v-btn color="orange darken-4" @click="dialogSlot = false">Cancel</v-btn>
+														</v-flex>	
+													</v-layout>	
+												</v-card-actions>
+											</v-card>
+										</v-dialog>	
+									</v-radio-group>
+								</v-flex>
+
+								<v-flex xs12>
+									<v-radio-group prepend-icon="fas fa-cookie" label="Futsal Court (1 Hr Slot)" v-model="radioGB2" row>
+										<v-radio label="Without Floodlight" value="radio-wf"></v-radio>
+										<v-radio label="With Floodlight" value="radio-f"></v-radio>
+										<span class="subheading orange--text">Select Slot</span>
+									</v-radio-group>
+								</v-flex>
+
+								<v-flex xs12>
+									<v-radio-group prepend-icon="fas fa-cookie" label="Basketball Court (1 Hr Slot)" v-model="radioGB3" row>
+										<v-radio label="Without Floodlight" value="radio-wf"></v-radio>
+										<v-radio label="With Floodlight" value="radio-f"></v-radio>
+										<span class="subheading orange--text">Select Slot</span>
+									</v-radio-group>
+								</v-flex>
+
+								<v-flex xs12>
+									<v-radio-group prepend-icon="fas fa-cookie" label="Tennis Court (1 Hr Slot)" v-model="radioGB4" row>
+										<v-radio label="Without Floodlight" value="radio-wf"></v-radio>
+										<v-radio label="With Floodlight" value="radio-f"></v-radio>
+										<span class="subheading orange--text">Select Slot</span>
+									</v-radio-group>
+								</v-flex>
+
+								<v-flex xs3>
+									<v-checkbox label="Cricket Nets (1 Hr)"></v-checkbox>
+								</v-flex>
+								<v-flex xs3>
+									<v-checkbox label="Wooden Floor"></v-checkbox>
+								</v-flex>
+							</v-layout>	
+						</v-card>
+						
+						<v-card width="100%" height="50px" color="#303030">
+							<div class="right pr-2">
+								<v-btn flat>Cancel</v-btn>
+								<v-btn @click="snackbar2 = true" color="orange darken-4" class="mb-2">Submit</v-btn>
+								<v-snackbar v-model="snackbar2" :bottom="y === 'bottom'">Ground Booking Done!
+									<v-btn color="orange darken-4" flat @click="snackbar2 = false">Close</v-btn>
+								</v-snackbar>
+							</div>
+						</v-card>		
         			</v-card>
       			</v-tab-item>
 			</v-tabs>	  
@@ -133,6 +249,12 @@ export default class Home extends Vue{
 		v => v.length <= 15 || "Name must be less than 15 characters",
 	]
 	radioGroup1 = 1
+	radioGB1 = 1
+	radioGB2 = 1
+	radioGB3 = 1
+	radioGB4 = 1
+	dialogSlot = false
+	toggle_none = null
 	items = [
 		'Programming',
 		'Design',
@@ -144,11 +266,19 @@ export default class Home extends Vue{
 		v => (v || '').match(/@/) || 'Please enter a valid email',
 	]
 	snackbar1 = false
+	snackbar2 = false
     y= 'top'
 	mode= ''
 	timeout= 6000
 	radioTop = 'radio-1'
 	radios1 = 'radio-1'
+	OrgType = [
+		'Schools',
+		'Grassroots',
+		'Corporate',
+		'Outsider Teams',
+		'Professional Teams'
+	]
 
 }
 </script>
