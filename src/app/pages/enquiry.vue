0ad5ccb8a48e31f84ab38413003d02d6ed2f2346
K Karthikeyan <kkarthik24.2012@gmail.com>
2019-01-24 17:16:08 +0530

    enquiry fxies done

--- src/app/pages/enquiry.vue
+++ src/app/pages/enquiry.vue
@@ -1,20 +1,20 @@
 <template>
 	<Layout>
 		<h1 class="text-md-center">Enquiry</h1>
-		<v-card class="mb-2" color="transparent" height="650px">
-			<v-layout row wrap>
+		<v-card class="mb-2" color="transparent" height="630px">
+			<v-layout row wrap class="pl-4">
 				<v-flex  class="px-1">
 					<v-text-field prepend-icon="fas fa-user" v-model="firstname" :rules="nameRules" counter maxlength="15" label="First Name" required></v-text-field>
 				</v-flex>
 				<v-flex  class="px-1">
 					<v-text-field counter maxlength="15" label="Middle Name"></v-text-field>
 				</v-flex>
-				<v-flex  class="px-1">
+				<v-flex  class="px-1 pr-4">
 					<v-text-field counter maxlength="15" label="Last Name"></v-text-field>
 				</v-flex>
 			</v-layout>
 
-			<v-layout row wrap>
+			<v-layout row wrap class="pl-4">
 				<v-flex xs12 lg6>
 					<v-radio-group prepend-icon="fas fa-transgender-alt" label="Gender" v-model="radioGroup2" row>
 						<v-radio label="Male" value="radio-4"></v-radio>
@@ -23,12 +23,12 @@
 					</v-radio-group>
 				</v-flex>
 
-				<v-flex xs12 lg6 class="pr-2">
+				<v-flex xs12 lg6 class="pr-4">
 					<v-combobox prepend-icon="work" :items="items" label="Occupation"></v-combobox>
 				</v-flex>
 			</v-layout>
 
-			<v-layout row wrap>
+			<v-layout row wrap class="pl-4">
 				<v-flex xs3 lg4 class="pr-2">
 					<v-text-field prepend-icon="fas fa-mobile-alt" v-model="phone" :rules="phoneRules" label="Mobile Number" mask="phone" required></v-text-field>
 				</v-flex>
@@ -40,25 +40,25 @@
 				</v-flex>
 			</v-layout>
 
-			<v-layout row wrap>
+			<v-layout row wrap class="pl-4">
 				<v-flex xs12 lg6 class="pr-2">
 					<v-textarea prepend-icon="place" name="input-7-1" label="Residential Address"></v-textarea>
 				</v-flex>
-				<v-flex xs6 lg6>
+				<v-flex xs6 lg6 class="pr-4">
 					<v-text-field prepend-icon="fas fa-envelope" v-model="email" :rules="emailRules" label="Email address" type="email"></v-text-field>
 				</v-flex>
 			</v-layout>
-			<v-flex xs12 lg6 class="pa-1">
+			<v-flex xs12 lg6 class="pl-4 pt-2">
 				<v-combobox v-model="select" :items="purposes" label="Purpose of Joining Gym" multiple chips hint="Maximum 3 choices"
 				 persistent-hint clearable deletable-chips></v-combobox>
 			</v-flex>
-			<h3>Type Of Membership</h3>
-			<v-layout row wrap xs6>
+			<h3 class="pl-4 pt-4">Type Of Membership</h3>
+			<v-layout row wrap xs6 class="pl-4">
 				<v-checkbox class="ml-4" label="Gold"></v-checkbox>
 				<v-checkbox label="Platinum"></v-checkbox>
 			</v-layout>
-			<h3>Membership Duration</h3>
-			<v-layout row wrap>
+			<h3 class="pl-4">Membership Duration</h3>
+			<v-layout row wrap class="pl-4">
 				<v-checkbox class="ml-4" label="Monthly"></v-checkbox>
 				<v-checkbox label="Quaterly"></v-checkbox>
 				<v-checkbox label="Half-Yearly"></v-checkbox>
