da273dc9980155b8f0562a5bb4e4bad6841d1153
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2019-01-26 01:43:15 +0530

    [added] graphql client

--- addons/neutron/config.ts
+++ addons/neutron/config.ts
@@ -1,12 +1,13 @@
 import path from "path"
 import BuildHelper from "~/build/helper"
+import { PORTS } from "@classes/ports"
 
 const TRANSPILE_DEST = BuildHelper.resolve("dist/neutron")
 const BIN_NAME = "neutron"
 
 export default {
 	config: {
-		port: 9101 // FIXME: change to 9102 on production
+		port: PORTS.NEUTRON
 	},
 	pack: {
 		input: path.resolve(TRANSPILE_DEST, "./index.js"),
