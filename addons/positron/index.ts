2cab2f52e650b6ef775841ef8f1fa40bd72b5b60
Nikhil Singh <lenikhilsingh@gmail.com>
2019-01-26 22:34:14 +0530

    [fixed] ServerBuilder for windows

--- addons/positron/index.ts
+++ addons/positron/index.ts
@@ -1,33 +1,14 @@
-import { compile } from "nexe"
-import path from "path"
-
-import BuildHelper from "~build/helper"
 import serverConfig from "~positron/build/webpack.prod"
 import config from "~positron/config"
+import ServerExecutableBuilder from "~/build/helpers/ServerBuilder"
 
-export default class ServerExecutableBuilder extends BuildHelper{
-	constructor(private buildPath: string, private platform: string) { super() }
-
-	public async build(){
-		let output = path.resolve(this.buildPath, ["linux", "win32", ].includes(this.platform) ? `resources/${config.pack.output}` : config.pack.output)
-		console.log("Building Server Executable")
-		console.info("path : ", output)
-		try {
-			await ServerExecutableBuilder.pack(serverConfig)
-			await compile({
-				...config.pack,
-				...{
-					output,
-					targets: [this.platform,],
-				},
-			})
-			return {
-				output,
-				targets: [ this.platform, ],
-			}
-		} catch (error) {
-			ServerExecutableBuilder.console.error("Server build failed because", error)
-			throw "Server Build Failure"
-		}
+export default class PositronBinaryBuilder extends ServerExecutableBuilder{
+	constructor(buildPath: string, platform: string){
+		super(buildPath, platform, {
+			Namespace: "positron/binary-builder",
+			ServerName: "Positron",
+			serverConfig,
+			config
+		})
 	}
 }
--- build/addons/positron.ts
+++ build/addons/positron.ts
@@ -1,8 +1,9 @@
 /**
  * TODO: build release system for the rest
  */
-import ServerExecutableBuilder from "~addons/positron/index"
 import { resolve } from "../webpack.base"
+import PositronBinaryBuilder from "~addons/positron"
+import os from "os"
 
-let positronBuilder  = new ServerExecutableBuilder(resolve("bin"), "linux")
+let positronBuilder = new PositronBinaryBuilder(resolve("bin"), os.platform())
 positronBuilder.build()
