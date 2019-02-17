315493b89eb594682922bd9fc5ded9c17bc423fe
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2019-02-17 06:21:21 +0530

    [added] Vue splitted script auto imports 'a.vue.ts' to 'a.vue' given 'a.vue' does not contain script

--- src/splashscreen/App.vue
+++ src/splashscreen/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-app dark>
-		<v-layout v-if="!x_error" align-center justify-space-between row wrap fill-height>
+		<v-layout v-if="!error" align-center justify-space-between row wrap fill-height>
 			<v-flex xs12> <loading style="bottom: 0px;" /> </v-flex>
 			<v-flex xs12>
 				<v-img :src="logo" class="white--text" contain max-height="100" />
@@ -24,39 +24,6 @@
 		</v-layout>
 	</v-app>
 </template>
-
-<script lang="ts">
-// @ts-ignore
-import logo from "@/assets/images/splash-logo.png"
-import { Component, Vue } from "vue-property-decorator"
-import Loading from "./Loading.vue"
-import { ipcRenderer } from "electron"
-
-@Component({
-	components: { Loading, },
-	created(){
-		ipcRenderer.on("splash-log", (event, args)=>{
-			this.x_message = args
-		})
-		ipcRenderer.on("splash-error", (event, args)=>{
-			this.x_error = "failed"
-			console.log(args)
-		})
-	},
-})
-export default class SplashcreenPage extends Vue{
-		logo = logo
-		x_message = "default message"
-		x_error = ""
-		get message(){ return this.x_message }
-		get error(){ return this.x_error }
-
-		exit(){
-			ipcRenderer.send("kill-me", 0)
-		}
-}
-</script>
-
 
 <style>
 body {
