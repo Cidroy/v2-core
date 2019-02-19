5034334eab506d4712786c14aed7b3bfdabf7f32
Nikhil Singh <lenikhilsingh@gmail.com>
2019-02-20 01:46:40 +0530

    [usual commit]

---
+++ classes/functions/utils.ts
@@ -0,0 +1,9 @@
+export default class Utils{
+	public static appendZeroesToBadgenumber( badgenumber: string): string{
+	
+		while (badgenumber.length < 9){
+			badgenumber = "0"+badgenumber
+		}
+		return badgenumber
+	}
+}
