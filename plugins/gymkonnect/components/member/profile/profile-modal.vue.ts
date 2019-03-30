99348318f52e0467470d09dfc826cc2d4c1d4cd5
Kundan Singh <kundan785@gmail.com>
2019-03-30 13:24:50 +0530

    profile

--- plugins/gymkonnect/components/member/profile/profile-modal.vue.ts
+++ plugins/gymkonnect/components/member/profile/profile-modal.vue.ts
@@ -1,9 +1,10 @@
 import { Component, Vue, Watch, Prop, Emit } from "vue-property-decorator"
-import empty from "@/components/empty.vue"
-
+import MRegistrationStepFinished from "../registration/step-finished.vue"
 @Component({
 	// @ts-ignore
-	components: { empty, },
+	components: {
+		MRegistrationStepFinished,
+	},
 })
 // @ts-ignore
 export default class ProfileModal extends Vue {
@@ -28,6 +29,75 @@ export default class ProfileModal extends Vue {
 	]
 	private cards = [
 		{ src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg", flex: 6 },
+	]
+
+	private headers = [
+		
+		{ text: "MembersShip Type", value: "name" },
+		{ text: "Package", value: "package", sortable: false },
+		{ text: "Start Date", value: "startDate", sortable: false },
+		{ text: "End Date", value: "endDate" },
+	]
+
+	private desserts = [
+		{
+			
+			name: "Master",
+			package: "$100",
+			startDate: "12/2/18",
+			endDate: "12/2/19"
+		},
+		{
+			
+			name: "Noob",
+			package: "$100",
+			startDate: "12/2/18",
+			endDate: "12/2/19"
+		},
+		{
+			
+			name: "standard",
+			package: "$100",
+			startDate: "12/2/18",
+			endDate: "12/2/19"
+		},
+		{
+			
+			name: "standard",
+			package: "$100",
+			startDate: "12/2/18",
+			endDate: "12/2/19"
+		},
+	]
+
+	private headers2 = [
+		
+		{ text: "Start Date", value: "name2" },
+		{ text: "End Date", value: "endDate" },
+		{ text: "Days", value: "days" },
+	]
+
+	private desserts2 = [
+		{
+			name2: "12/2/18",
+			endDate: "12/2/19",
+			days: 1
+		},
+		{
+			name2: "17/2/18",
+			endDate: "12/2/19",
+			days: 4
+		},
+		{
+			name2: "19/2/18",
+			endDate: "02/2/19",
+			days: 6
+		},
+		{
+			name2: "10/2/18",
+			endDate: "23/2/19",
+			days: 9
+		},
 	]
 	// #endregion
 }
