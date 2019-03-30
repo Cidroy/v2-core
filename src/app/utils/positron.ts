71052f24505186b40b8418826e2f2459d2ea6e0e
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2019-03-30 19:48:41 +0530

    [fixed] profile photo

---
+++ src/app/utils/positron.ts
@@ -0,0 +1,31 @@
+import { Logger } from "@classes/CONSOLE"
+import { PORTS } from "@classes/ports"
+import AppConfig from "@classes/appConfig"
+
+let client = undefined
+
+// TODO: [Vicky] implement positron client
+export default class PositronClient{
+	private static Namespace = "electron/ui"
+	private static log = new Logger(PositronClient.Namespace)
+
+	protected static config = {
+		host: "localhost",
+		path: "",
+		port: PORTS.POSITRON,
+		ssl: false,
+	}
+
+	public static get uri() {
+		return `http${PositronClient.config.ssl ? "s" : ""}://${PositronClient.config.host}${PositronClient.config.port ? ":" + PositronClient.config.port : ""}${PositronClient.config.path||""}`
+	}
+
+	public static async Initialize(force: boolean = false){
+		if (!force && client) return true
+		PositronClient.log.verbose("initialize")
+		PositronClient.config = (await AppConfig.Get(PositronClient.Namespace, {
+			positron: PositronClient.config
+		})).positron
+		return true
+	}
+}
