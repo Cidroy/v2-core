19625c244b9f8d53e163b4881fdc54dac9a2fbdf
Rinzler D. Vicky <rinzler.d.vicky@gmail.com>
2019-05-24 02:30:36 +0530

    [todo] implement resource manager

--- src/app/components/dev-resizer.vue
+++ src/app/components/dev-resizer.vue
@@ -1,10 +1,15 @@
 <template>
-    <v-dialog v-model="modal" max-width="200">
+    <v-dialog v-model="modal" max-width="300">
         <v-card>
-            <v-card-title class="headline">Resize Application</v-card-title>
             <v-card-text>
-				<v-btn block @click.native.stop="mobile">Mobile</v-btn>
-				<v-btn block @click.native.stop="fullScreen">Full</v-btn>
+	            <v-card-title class="headline">Developer Options</v-card-title>
+				<v-subheader>Resize</v-subheader>
+				<v-layout row wrap>
+					<v-flex xs6 class="px-1"><v-btn block @click.native.stop="mobile"> <v-icon left>smartphone</v-icon> Mobile</v-btn></v-flex>
+					<v-flex xs6 class="px-1"><v-btn block @click.native.stop="fullScreen"><v-icon left>fullscreen</v-icon> Full</v-btn></v-flex>
+				</v-layout>
+				<v-subheader>Advanced</v-subheader>
+				<v-btn block @click.native.stop="showTaskManager"> <v-icon left>dvr</v-icon> Task Manager</v-btn>
             </v-card-text>
         </v-card>
     </v-dialog>
@@ -23,4 +28,18 @@ export default class DevResizer extends Vue.default {
 	@Watch("show") private onShowChanged(){ this.modal = this.show }
 	private mobile(){ ResizeWindow.mobileSize() }
 	private fullScreen(){ ResizeWindow.fullSize() }
-}</script>
+
+	private async showTaskManager(){
+		// #!if electron
+		/**
+		 * BUG: import is not working as expected
+		 * expected import to be a function got an empty object
+		 */
+		const openProcessManager = await import("electron-process-manager")
+		console.log(openProcessManager)
+		// #!else
+		alert("this is only available on Desktop version")
+		// #!endif
+	}
+}
+</script>
