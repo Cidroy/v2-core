7272f63c030cfc7966847724d57f658d25aadf05
Kundan Singh <kundan785@gmail.com>
2019-06-28 12:37:11 +0530

    [patch]

--- classes/utils/webworker-thread/index.js
+++ classes/utils/webworker-thread/index.js
@@ -2,7 +2,7 @@ import os from "os"
 
 var WebWorkerThreads
 if(os.platform()==="win32") WebWorkerThreads = require("./WebWorkerThreads-win32.node")
-if(os.platform()==="linux") WebWorkerThreads = require("./WebWorkerThreads-win32.node")
+if(os.platform()==="linux") WebWorkerThreads = require("./WebWorkerThreads-linux.node")
 
 
 export default WebWorkerThreads
